63d3285d64ab4e5214aa9ee93e063d5f55742577
Narendra Jaggi <jagginarendra@gmail.com>
2018-01-10 07:34:52 +0530

    DP rod cutting problem solved

--- RodCuttingDP.java
+++ RodCuttingDP.java
@@ -11,8 +11,8 @@ public class RodCuttingDP {
         for(int i = 0 ; i < N ; i++){
             prices[i] = scanner.nextInt();
         }
-        int values[] = new int[N];
-        for(int i = 0 ; i < N ; i++){
+        int values[] = new int[N+1];
+        for(int i = 1 ; i < N+1 ; i++){
             values[i] = scanner.nextInt();
         }
 
@@ -22,17 +22,16 @@ public class RodCuttingDP {
 
     private static int maxProfit(int totalRodLength,int N, int[] rodLength, int[] values) {
 
-        int array[][] = new int[N+1][N+1];
+        int array[][] = new int[N+1][totalRodLength+1];
 
-        for(int i = 1 ; i < values.length ; i++){
-            for (int j=1 ; j < rodLength.length ;  j++){
+        for(int i = 1 ; i < N+1 ; i++){
+            for (int j=1 ; j < totalRodLength+1 ;  j++){
 
-                array[i][j] = Integer.max(array[i-1][j] , array);
+                array[i][j] = Integer.max(array[i-1][j] ,
+                        j/i*values[i] + array[i][j%i]);
 
             }
           }
-
-
-        return 0;
+        return array[N][totalRodLength];
     }
 }
